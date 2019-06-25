2741c11c405b5370053f54012906d26160af32d7
Taylor Brushwyler <taylor.brushwyler@toro.com>
2019-06-25 15:13:41 -0400

    Add TapCommandBehavior and BaseAttachable

---
+++ Library/PhantomLib/Models/IAttachable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PhantomLib.Models
+{
+    /// <summary>
+    /// Interface for classes that will notify when their properties are changing
+    /// or have changed.
+    /// </summary>
+    public interface IAttachable : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+
+    }
+}
