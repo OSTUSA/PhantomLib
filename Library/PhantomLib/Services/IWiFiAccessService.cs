323f0cfc6e68ccd347953bf441713ed9ceb4371b
Arie Williams <awilliams@ostusa.com>
2020-01-09 14:33:46 -0500

    Adding in Wifi Service
    Adding in Wifi Service to android and iOS

---
+++ Library/PhantomLib/Services/IWiFiAccessService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PhantomLib.Services
+{
+    public interface IWiFiAccessService
+    {
+        void WiFiAccessEnabled(string ssid, string networkPassword);
+    }
+}
