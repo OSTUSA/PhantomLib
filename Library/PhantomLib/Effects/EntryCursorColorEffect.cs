a222bc05c0335dc5bbda52bf4a27d9617bb80f22
Cody Pearson <cody.m.pearson@gmail.com>
2020-05-14 12:16:50 -0400

    Added EntryCursorColorEffect for iOS on EffectsPage

---
+++ Library/PhantomLib/Effects/EntryCursorColorEffect.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace PhantomLib.Effects
+{
+    public class EntryCursorColorEffect : RoutingEffect
+    {
+        public Color CursorColor { get; set; }
+
+        public EntryCursorColorEffect() : base($"{ResolutionGroupName.PhantomLib}.{nameof(EntryCursorColorEffect)}") { }
+    }
+}
